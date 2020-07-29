e3c23d0ef3501ed28a0d794ceb6b3b4e472eeb30
angelafu <angelinmay@hotmail.com>
2020-07-29 19:31:45 +1000

    Add files via upload

---
+++ book_list.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Books sorted by latest purchase date</title>
+</head>
+<body>
+<table>
+    @foreach($subsetWithBookCategory as $item)
+    <tr>
+        <td>{{$item.ISBN}}</td>
+        <td>
+            <a href="book_purchases/{{item->id}}">
+                {{$item.book_name}}
+            </a>
+        </td>
+        <td>{{$item.category_name}}</td>
+        <td>{{$item.latest_purchase_date}}</td>
+    </tr>
+    @endforeach
+</table>
+</body>
+</html>
